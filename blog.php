310d9c83c7b24cd25158009d0f806fb58d76df73
Kevin Chevreuil - Kaisen <kaisen@kaisen-linux.org>
2021-03-07 22:06:41 +0100

    Adding blog and CSS updates

---
+++ blog.php
@@ -0,0 +1,62 @@
+<?php include('./header.php');?>
+<?php include('./__blog.php');?>
+
+
+
+<!-- Blog Grid START -->
+<div class="section-block-grey blogging">
+  <div class="container">
+    <div class="section-heading">
+      <h4>Latest News</h4>
+      <div class="section-heading-line-left"></div>
+    </div>
+    
+    <div class="row mt-30">
+
+
+
+    <?php foreach ($blog as $key_b => $b) { ?>
+      <div class="col-md-4 col-sm-6 col-12">
+
+        <div class="blog-grid-simple">
+      
+        <img src="<?php echo $b['visuel']; ?>">
+
+        <h4><?php echo $b['title']; ?></h4>
+        <p><?php echo $b['extract']; ?></p>
+        <div class="blog-grid-simple-content">
+          <div class="row blog-grid-simple-date">
+            <div class="col-2 pr-0">
+              <i class="fa fa-calendar"></i>
+            </div>
+            <div class="col-5 pl-0">
+              <h5><?php echo $b['date']; ?></h5>
+            </div>
+            <div class="col-5 right-holder">
+              <a href="./blog/<?php echo $b['link']; ?>">Read on blog</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    <?php  } ?>
+
+
+
+
+
+
+
+    </div>
+  </div>
+</div>
+<!-- Blog Grid END -->
+
+</div>
+<!-- Project Detail Section END -->
+
+<script type="text/javascript"> var mySwiper = new Swiper('.swiper-container', { direction: 'horizontal' })</script>
+
+<?php include('./footer.php');?>
---
+++ blog/kaisenlinux20200304.php
@@ -0,0 +1,54 @@
+<?php include('../header.php');?>
+
+<div id="content" class="single-blog">
+    
+
+
+<section class="section">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 offset-lg-2 text-center">
+        <h1>Kaisen Linux 20200304 Release Notes</h1>
+        <ul class="list-inline mb-50">
+          <li class="list-inline-item">Author: Kaisen</li>
+          <li class="list-inline-item">Date: 2020/04/03</li>
+        </ul>
+        <img class="img-fluid mb-50" src="../img/screen.png" alt="blog-image">
+      </div>
+      <div class="col-lg-8 offset-lg-2">
+        <div class="post-single-content">
+          <h1 id="kaisenlinux20200304">Kaisen Linux 20200304 changelog</h1>
+<p>This new release is revision of version 20200301. The exfat filesystem is now supported. Important bug fixed.</p>
+<p>Codename: Armageddon</p>
+<p>Softwares updates:</p>
+<ul>
+<li>Linux upgrade to 5.4.9 version (with most optimisation)</li>
+</ul>
+<p>Added softwares and hardware support:</p>
+<ul>
+<li>exfat driver supported (experimental)</li>
+<li>etherape (thanks to jergud for suggestion)</li>
+<li>vscode</li>
+<li>arpwatch</li>
+<li>arpalert</li>
+<li>wakeonlan</li>
+<li>driftnet</li>
+<li>ettercap</li>
+</ul>
+<p>Graphical interface:</p>
+<ul>
+<li>arpwatch section added in services menu</li>
+</ul>
+<p>Fix bugs:<p>
+<ul>
+<li>Failsafe mode fixed (initrd not charging because nolapic and nomodeset not functional in 5.x kernel) 
+in default and charging in RAM mode</li>
+</ul>
+        </div>
+
+    </div>
+  </div>
+</section>
+
+</div><!-- end Contact Area -->
+<?php include('../footer.php');?>
