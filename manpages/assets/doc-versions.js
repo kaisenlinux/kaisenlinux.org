7a82225979362b4a6d6af247d623831714fb4ad0
Arnaud Cormier <magiknono@kaisenlinux.org>
2021-03-07 20:39:10 +0100

    Updating manpages. Made category manpages by default & updating readme

--- manpages/assets/doc-versions.js
+++ manpages/assets/doc-versions.js
@@ -1,10 +1,6 @@
 var versionNodes = [
   {
     version: "sort by packages", // version number or name (required)
-    url: "https://kaisenlinux.org/manpages/readme.html" // documentation URL (required)
-  },
-  {
-  version: "sort by category", 
-  url: "https://manpages.kaisen-linux.org/manpages/category/readme.html"
+    url: "https://kaisenlinux.org/manpages/packages/readme.html" // documentation URL (required)
   }
 ]
---
+++ manpages/packages/packages/doc-versions.js
@@ -0,0 +1,6 @@
+var versionNodes = [
+    {
+    version: "sort by category", 
+    url: "https://manpages.kaisen-linux.org/manpages/readme.html"
+    }
+  ]
