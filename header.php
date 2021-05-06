7137225b3c45001abd6a5ce62c503e09dff3459d
Kevin Chevreuil - Kaisen <kaisen@kaisenlinux.org>
2021-05-06 02:44:14 +0200

    Add mastodon and telegram links

--- header.php
+++ header.php
@@ -91,8 +91,10 @@
           	<li><a href="https://gitlab.com/kaisenlinux" target="_blank">Gitlab</a></li>
           	<li><a href="https://github.com/kaisenlinux" target="_blank">Github (Gitlab mirror)</a></li>
           	<li><a href="https://linkedin.com/company/kaisenlinux" target="_blank">Linkedin</a></li>
-          	<li><a href="https://twitter.com/kaisenlinux" target="_blank">Twitter</a></li>
-        	
+          	<li><a href="https://twitter.com/KaisenLinux" target="_blank">Twitter</a></li>
+		<li><a href="https://mastodon.social/@KaisenLinux" target="_blank">Mastodon</a></li>
+        	<li><a href="https://t.me/KaisenLinux" target="_blank">Telegram</a></li>
+
           </ul>
 
 	      </li>
