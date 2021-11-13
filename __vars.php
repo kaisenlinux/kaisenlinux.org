c047022fbe9747ac06b5df7bdc77184b66c7b0a2
Kevin Chevreuil - Kaisen <kaisen@kaisenlinux.org>
2021-11-13 05:44:54 +0100

    Add 2.0RC4 checksums ISOS and update ISO sizes.

--- __vars.php
+++ __vars.php
@@ -18,7 +18,7 @@
 
 			'include' => 'All tools included',
 			'img'	  => './img/downloads/mate.png',
-			'size' => '4.2Go',
+			'size' => '4.3Go',
 
 			'links' => [
 				[
@@ -38,7 +38,7 @@
 
 			'include' => 'All tools included',
 			'img'	  => './img/downloads/kde.png',
-			'size' => '4.6Go',
+			'size' => '4.7Go',
 
 			'links' => [
 				[
@@ -58,7 +58,7 @@
 
 			'include' => 'All tools included',
 			'img'	  => './img/downloads/xfce.png',
-			'size' => '4.0Go',
+			'size' => '4.2Go',
 
 			'links' => [
 				[
@@ -78,7 +78,7 @@
 
 			'include' => 'All tools included',
 			'img'	  => './img/downloads/lxqt.png',
-			'size' => '4.1Go',
+			'size' => '4.2Go',
 
 			'links' => [
 				[
@@ -98,7 +98,7 @@
 
 			'include' => 'No installer, live only',
 			'img'	  => './img/downloads/sr.png',
-			'size' => '2.4Go',
+			'size' => '2.5Go',
 
 			'links' => [
 				[
@@ -118,7 +118,7 @@
 
 			'include' => 'Network required',
 			'img'	  => './img/downloads/netinst.png',
-			'size' => '392Mo',
+			'size' => '459Mo',
 
 			'links' => [
 				[
