4fcb2443cd03f1be0397080da14caccb143366bc
Arnaud Cormier <magiknono@kaisenlinux.org>
2023-03-05 19:44:20 +0100

    update manpages 2.2 with Configuration_files (man5)

--- manpages/doc-versions.js
+++ manpages/doc-versions.js
@@ -1,6 +1,6 @@
 var versionNodes = [
   {
     version: "Sort by commands", // version number or name (required)
-    url: "https://kaisenlinux.org/manpages/commands/readme.html" // documentation URL (required)
+    url: "file:///usr/share/kaisen-manpages/commands/readme.html" // documentation URL (required)
   }
 ]
