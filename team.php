bbdb275a79fb226d5ba070c903b041b38b602d1c
Nicolas Béjean <nicolas@bejean.fr>
2023-03-17 17:47:45 +0100

    fix: fix bootstrap classes and migrate data to array

--- team.php
+++ team.php
@@ -1,5 +1,34 @@
 <?php include('./header.php');?>
 
+<?php
+$members = [
+    [
+        'img' => 'img/team/labrute.jpg',
+        'name' => 'Mehdi Lecomte',
+        'nickname' => 'LaBrute!',
+        'job' => 'Core developer',
+        'text' => 'Linux sysadmin, he is the core developer of Kaisen Linux. Package and infrastructure maintainer, He is also in charge of user support. He also participates in the decision making on the evolution of Kaisen.',
+        'linkedin_url' => 'https://www.linkedin.com/in/b12644a6/'
+    ],
+    [
+        'img' => 'img/team/tobas.jpg',
+        'name' => 'Tomi Bequet',
+        'nickname' => 'tobas',
+        'job' => 'Graphic designer',
+        'text' => 'Freelance graphic designer for several years, he provides us with our wallpapers as well as his expertise on the user experience. He is the one who reworked our official logo.',
+        'linkedin_url' => 'https://www.linkedin.com/in/tomibequet/'
+    ],
+    [
+        'img' => 'img/contributors/kaisen.jpg',
+        'name' => 'Arnaud Cormier',
+        'nickname' => 'Magiknono',
+        'job' => 'Manpages maintainer',
+        'text' => 'Freelance Linux sysadmin and webdev. He contributed a lot on Kaisen Linux with videos. Today, he is maintainer of &#x3C;a href=&#x22;manpages/&#x22;&#x3E;manpages&#x3C;/a&#x3E; documentation.',
+        'linkedin_url' => 'https://www.linkedin.com/in/arnaudcormier/'
+    ],
+];
+?>
+
 <!-- Team Members START -->
 <div class="section-block-grey">
   <div class="container">
@@ -9,8 +38,8 @@
       <p>and we are delighted to be able to provide you with a
          complete tool like this.</p>
     </div>
-    <div class="row mt-50  justify-content-md-center">
-      <div class="col-md-3 col-sm-6 col-12">
+    <div class="row mt-50 justify-content-sm-center">
+      <div class="col-12 col-sm-6 col-lg-4">
         <div class="team-member">
           <div class="team-member-img">
             <img src="img/team/kaisen.jpg" alt="img">
@@ -26,59 +55,26 @@
           </div>
         </div>
       </div>
+    </div>
 
-      <div class="row mt-50  justify-content-md-center">
-      <div class="col-md-3 col-sm-6 col-12">
-        <div class="team-member">
-          <div class="team-member-img">
-            <img src="img/team/labrute.jpg" alt="img">
-          </div>
-          <div class="team-member-text">
-            <h4>Mehdi Lecomte<br><small>LaBrute!</small></h4>
-            <span>Core developer</span>
-            <p>Linux sysadmin, he is the core developer of Kaisen Linux. Package and infrastructure maintainer,
-            He is also in charge of user support. He also participates in the decision making on the evolution of Kaisen.<p>
-            <ul>
-              <li><a href="https://www.linkedin.com/in/b12644a6/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
-            </ul>
-          </div>
+    <div class="row mt-50 justify-content-sm-center">
+        <?php foreach ($members as $member): ?>
+        <div class="col-12 col-sm-6 col-lg-4">
+            <div class="team-member">
+                <div class="team-member-img">
+                    <img src="<?php echo($member['img']);?>" alt="img">
+                </div>
+                <div class="team-member-text">
+                    <h4><?php echo($member['name']);?><br><small><?php echo($member['nickname']);?></small></h4>
+                    <span><?php echo($member['job']);?></span>
+                    <p><?php echo(html_entity_decode($member['text']));?><p>
+                    <ul>
+                        <li><a href="<?php echo($member['linkedin_url']);?>" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
-      </div>
-
-      <div class="col-md-3 col-sm-6 col-12">
-        <div class="team-member">
-          <div class="team-member-img">
-            <img src="img/team/tobas.jpg" alt="img">
-          </div>
-          <div class="team-member-text">
-            <h4>Tomi Bequet<br><small>tobas</small></h4>
-            <span>Graphic designer</span>
-            <p>Freelance graphic designer for several years, he provides us with our wallpapers as well as his 
-               expertise on the user experience. He is the one who reworked our official logo.</p>
-
-            <ul>
-              <li><a href="https://www.linkedin.com/in/tomibequet/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-            <div class="col-md-3 col-sm-6 col-12">
-        <div class="team-member">
-          <div class="team-member-img">
-            <img src="img/contributors/kaisen.jpg" alt="img">
-          </div>
-          <div class="team-member-text">
-            <h4>Arnaud Cormier<br><small>Magiknono</small></h4>
-            <span>Manpages maintainer</span>
-            <p>Freelance Linux sysadmin and webdev. He contributed a lot on Kaisen Linux
-               with videos. Today, he is maintainer of <a href="manpages/">manpages</a> documentation.</p>
-
-            <ul>
-              <li><a href="https://www.linkedin.com/in/arnaudcormier/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
+        <?php endforeach; ?>
     </div>
   </div>
 </div>
